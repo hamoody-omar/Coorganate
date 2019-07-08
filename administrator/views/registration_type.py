02af51358935e08b7de2b7a7227715f946051800
Mahamat Oumar <omar.mho20@gmail.com>
2019-07-08 07:53:35 -0500

    fixed registration issue

--- administrator/views/registration_type.py
+++ administrator/views/registration_type.py
@@ -7,6 +7,7 @@ Created on Jul 7, 2019
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from administrator.forms import UserRegisterForm, MedicalProfessionalForm, OrganizationForm, AddressForm
 from medical_professional.models import Organization, MedicalProfessional, MedicalProfessionalOrganization, Address, OrganizationAddress
@@ -28,16 +29,31 @@ def medical_professional_registration(request):
         user_form = UserRegisterForm(request.POST)
         med_pro_form = MedicalProfessionalForm(request.POST)
 
+        print("user form ", user_form.is_valid())
+        print("med form ", med_pro_form.is_valid())
+
         # Validate forms
         if user_form.is_valid() and med_pro_form.is_valid():
             user_form.save()
-            med_pro_form.save()
-            print("validated")
 
-            # Get medical professional and associate it with their organziation
-            # username = user_form.cleaned_data.get('username')
-            # med_pro = MedicalProfessional.objects.get(
-            #     user__username=username)
+            # Get user and associate with medical professional
+            username = user_form.cleaned_data.get('username')
+            user = User.objects.get(username=username)
+
+            license_ID = med_pro_form.cleaned_data.get('license_ID')
+            cellphone_number = med_pro_form.cleaned_data.get(
+                'cellphone_number')
+            work_phone_number = med_pro_form.cleaned_data.get(
+                'work_phone_number')
+
+            med_pro = MedicalProfessional()
+            med_pro.user = user
+            med_pro.license_ID = license_ID
+            med_pro.cellphone_number = cellphone_number
+            med_pro.work_phone_number = work_phone_number
+            med_pro.save()
+
+            # Associate medical professional with their organziation
             # organization = Organization.objects.get(
             #     id=int(request.POST['organization']))
             # med_pro_organization = MedicalProfessionalOrganization()
@@ -47,12 +63,14 @@ def medical_professional_registration(request):
 
             messages.success(
                 request, f'Thank you for your request. We will review your form and get back to you as soon as possible.')
-            return redirect('/request_review_message/')
+            return redirect('/request_review_message')
 
+    messages.success(
+        request, f'Something was wrong. Make sure your password is not too similar to your username.')
     context_dict = {}
     # Get the forms and render the medical professional registration form
     context_dict['user_form'] = UserRegisterForm()
-    context_dict['med_pro_form'] = MedicalProfessionalForm()
+    context_dict['med_pro_form'] = MedicalProfessionalForm(request.POST)
     # context_dict['Organization'] = Organization.objects.filter(
     #    is_approved=True)
 
@@ -90,7 +108,7 @@ def organization_registration(request):
             messages.success(
                 request, f'Thank you for your request. We will review your form and get back to you as soon as possible.')
 
-            return redirect('/request_review_message/')
+            return redirect('/request_review_message')
     else:
 
         # Get the forms and render the organization registration form
